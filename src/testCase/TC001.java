4686d713b96fec192613fce9d9befee0cf2f66a9
shaghayegh <shaghayeghfallah2022@gmail.com>
2022-04-06 13:05:23 -0400

    negative testing

--- src/testCase/TC001.java
+++ src/testCase/TC001.java
@@ -4,5 +4,6 @@ public class TC001 {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("more testing");
+        System.out.println("negative changing");
     }
 }
